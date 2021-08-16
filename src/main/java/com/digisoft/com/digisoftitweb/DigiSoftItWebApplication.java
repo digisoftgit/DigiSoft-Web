aac1889f6f7d99d506290538ee9cad65531db635
Digi soft <info@digisoft-it.com>
2021-08-16 16:15:11 +0400

    16-08-2021 adding security

--- src/main/java/com/digisoft/com/digisoftitweb/DigiSoftItWebApplication.java
+++ src/main/java/com/digisoft/com/digisoftitweb/DigiSoftItWebApplication.java
@@ -1,5 +1,14 @@
 package com.digisoft.com.digisoftitweb;
 
+import com.digisoft.com.digisoftitweb.security.entity.position.response.PositionsResponse;
+import com.digisoft.com.digisoftitweb.security.entity.role.request.RoleRequest;
+import com.digisoft.com.digisoftitweb.security.mapper.PositionsMapper;
+import com.digisoft.com.digisoftitweb.security.repository.PositionsRepository;
+import com.digisoft.com.digisoftitweb.security.services.positions.impl.CommandPositionServiceImplementation;
+import com.digisoft.com.digisoftitweb.security.services.positonsCategory.impl.PositionCategoryServiceImpl;
+import com.digisoft.com.digisoftitweb.security.services.role.CommandRoleService;
+import com.digisoft.com.digisoftitweb.security.services.user.impl.CommandUserServiceImplementation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,10 +19,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
+@RequiredArgsConstructor
 public class DigiSoftItWebApplication extends SpringBootServletInitializer implements CommandLineRunner {
+
+    private final PositionCategoryServiceImpl positionsCategoryServiceImpl;
+    private final CommandPositionServiceImplementation positionsService;
+    private final CommandUserServiceImplementation commandUserImplementation;
+    private final CommandRoleService roleControllerService;
+    private final PositionsMapper positionsMapper;
+    private final PositionsRepository positionsRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(DigiSoftItWebApplication.class, args);
@@ -36,7 +54,12 @@ public class DigiSoftItWebApplication extends SpringBootServletInitializer imple
 
     @Override
     public void run(String... args) throws Exception {
-
+        List<PositionsResponse> allPositions = positionsMapper.toResponse(positionsRepository.findAll());
+        if (allPositions.isEmpty()) {
+            roleControllerService.createAdminRole( RoleRequest.builder().name("admin").build());
+            positionsCategoryServiceImpl.fillData();
+            commandUserImplementation.addAdmin();
+        }
     }
 
 }
--- src/main/java/com/digisoft/com/digisoftitweb/security/services/positonsCategory/impl/PositionCategoryServiceImpl.java
+++ src/main/java/com/digisoft/com/digisoftitweb/security/services/positonsCategory/impl/PositionCategoryServiceImpl.java
@@ -1,20 +1,30 @@
 package com.digisoft.com.digisoftitweb.security.services.positonsCategory.impl;
 
+import com.digisoft.com.digisoftitweb.security.entity.positionscategory.PositionsCategories;
+import com.digisoft.com.digisoftitweb.security.repository.PositionCategoryRepository;
 import com.digisoft.com.digisoftitweb.security.services.positonsCategory.PositionsCategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class PositionCategoryServiceImpl implements PositionsCategoryService {
-
+    private final PositionCategoryRepository positionCategoryRepository;
     private final String[] positionCategories = {"Administration", "Students", "Teachers"};
-
 
     @Override
     public void fillData() {
-
+        List<PositionsCategories> allCategories = positionCategoryRepository.findAll();
+        if (allCategories.isEmpty()) {
+            for (String positionCategory : positionCategories) {
+                PositionsCategories positionsCategories = new PositionsCategories();
+                positionsCategories.setPositionCategoryName(positionCategory);
+                positionCategoryRepository.save(positionsCategories);
+            }
+        }
     }
 }
